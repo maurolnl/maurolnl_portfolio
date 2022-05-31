73ea4b4e7c8bdfd85df6621d39f72f396c006e1f
maurolnl <mauroleoquiroga@gmail.com>
2022-05-31 15:16:35 -0300

    Update InternalLink.tsx
    add hover prop

--- components/Layout/InternalLink.tsx
+++ components/Layout/InternalLink.tsx
@@ -1,20 +1,51 @@
 import React from "react";
 import NextLink from "next/link";
 import {Link} from "@chakra-ui/react";
+import {motion} from "framer-motion";
 
 interface Props {
   href: string;
   children?: React.ReactNode;
   variant?: string;
   isExternal?: boolean;
+  isHoverable?: boolean;
+  onClick?: () => void;
 }
 
-const InternalLink: React.FC<Props> = ({href, variant, isExternal, children}) => {
+const InternalLink: React.FC<Props> = ({
+  href,
+  variant,
+  isExternal,
+  isHoverable,
+  onClick,
+  children,
+}) => {
+  const MotionLink = motion(Link);
+
   return (
     <NextLink passHref href={href}>
-      <Link _hover={{textDecoration: "none"}} isExternal={isExternal} variant={variant}>
-        {children}
-      </Link>
+      {!isHoverable ? (
+        <MotionLink
+          _hover={{textDecoration: "none"}}
+          isExternal={isExternal}
+          variant={variant}
+          onClick={onClick}
+        >
+          {children}
+        </MotionLink>
+      ) : (
+        <MotionLink
+          _hover={{textDecoration: "none"}}
+          borderBottom="2px solid transparent"
+          isExternal={isExternal}
+          transition={{duration: 1, ease: "easeInOut"}}
+          variant={variant}
+          whileHover={{borderBottom: "2px solid black"}}
+          onClick={onClick}
+        >
+          {children}
+        </MotionLink>
+      )}
     </NextLink>
   );
 };
